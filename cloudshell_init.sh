b83fbb8434ca16d4b9090f0a3ab870c759ef559c
RJ.Wang <wangrenjun@gmail.com>
2025-03-28 16:31:21 +0800

    init

---
+++ cloudshell_init.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# AWS cloud shell init env
+
+# install eksctl
+# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+#
+# # (Optional) Verify checksum
+# curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+#
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+#
+sudo mv /tmp/eksctl /usr/local/bin
+
+
+# Installing kubectl
+# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Install Terraform
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+
+# install some tools
+sudo yum -y install dnsutils htop iftop wget tcpdump telnet lshw mtr dmidecode amazon-efs-utils
